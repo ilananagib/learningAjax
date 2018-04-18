cebb407b009be0951abc98d867e49cf6594212af
Ilana Nagib <ilana.nagib@yahoo.com>
2018-04-18 14:15:59 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require('express'); //connects to node express
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = 5000; //5000 is the port. It can be any number. Commomly used numbers are 3000 or 5000.
+const quotes_data = require('./quotes');
 
-app.listen(port, () => {
-    console.log('listening on port', port);
+app.use(express.static('server/public')); // line to connect server to public files
+
+app.get('/quotes', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * quotes_data.length);
+    res.send(quotes_data[randomNumber]);
 });
 
-app.get('/', function(req, res){
+app.get('/quotes', (req, res) => {
+    res.send(quotes_data);
+});
+
+app.get('/', (req, res) =>{
     res.send('happy wednesday');
 });
 
-const quotes_data = require('./quotes');
-console.log(quotes_data);
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
+
+
+
