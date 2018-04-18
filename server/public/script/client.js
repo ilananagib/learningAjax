0d557b642f3f0357068c40f2c24dd5c4b682a6c1
Ilana Nagib <ilana.nagib@yahoo.com>
2018-04-18 16:17:50 -0500

    final commit of the day

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -3,6 +3,27 @@ console.log('Client is loaded');
 $(document).ready(onReady);
     
 function onReady(){
-    console.log('jQuery has been loaded');
-}
+    $.ajax({
+        type: 'GET',
+        url: '/quotes'
+    })
+    .then(function(response) {
+        $('#randomQuote').text(response.quote + ' ' + response.author);
+       
+    });
 
+    console.log('jQuery has been loaded');
+    $('#quoteButton').on('click', clickAjax);
+
+    function clickAjax(){
+    $.ajax({
+        type: 'GET',
+        url: '/quotes'
+    })
+    .then(function(response) {
+        $('#randomQuote').text(response.quote + ' ' + response.author);
+    
+        
+    });
+}
+}
