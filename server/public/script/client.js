9e01552378d7bc38373a4a6ebcdecc7a5264f7dc
Ilana Nagib <ilana.nagib@yahoo.com>
2018-04-19 19:21:42 -0500

    final commit

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,29 +1,48 @@
 console.log('Client is loaded');
 
 $(document).ready(onReady);
-    
-function onReady(){
-    $.ajax({
-        type: 'GET',
-        url: '/quotes'
-    })
-    .then(function(response) {
-        $('#randomQuote').text(response.quote + ' ' + response.author);
-       
-    });
 
+function onReady() {
     console.log('jQuery has been loaded');
     $('#quoteButton').on('click', clickAjax);
+    $('#submitButton').on('click', addNewQuote);
+    getAllQuotes();
+}
 
-    function clickAjax(){
+function getAllQuotes() {
     $.ajax({
         type: 'GET',
         url: '/quotes'
     })
-    .then(function(response) {
-        $('#randomQuote').text(response.quote + ' ' + response.author);
-    
-        
+        .then(function (response) {
+            $('#randomQuote').text(response.quote + ' ' + response.author);
+
+        });
+}
+
+function clickAjax() {
+    $.ajax({
+        type: 'GET',
+        url: '/quotes'
+    })
+        .then(function (response) {
+            $('#randomQuote').text(response.quote + ' ' + response.author);
+
+
+        });
+}
+
+function addNewQuote() {
+    const newQuoteObj = { quote: $('#newQuote').val(), author: $('#newAuthor').val() };
+    console.log(newQuoteObj);
+    $.ajax({
+        method: 'POST',
+        url: '/add-quote',
+        data: newQuoteObj
+    })
+    .then(function (response) {
+        console.log(response);
+        getAllQuotes();
     });
 }
-}
+
